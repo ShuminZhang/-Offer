8a86850511a740d8ef9a837efce685d1d8261e36
Ciaran <mgqclseyn@163.com>
2022-03-01 21:29:04 +0800

    Offer05

---
+++ Offer05.java
@@ -0,0 +1,29 @@
+
+/*请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+示例 1：
+
+输入：s = "We are happy."
+输出："We%20are%20happy."
+*/
+import java.util.*;
+
+public class Offer05 {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ')
+                sb.append("%20");
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Offer05 o = new Offer05();
+        System.out.println(o.replaceSpace(str));
+        sc.close();
+    }
+}
