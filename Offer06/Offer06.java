f76ada12f249791bbb8bee412edca814b7189450
Ciaran <mgqclseyn@163.com>
2022-03-03 21:52:53 +0800

    2022.3.3

--- Offer06/Offer06.java
+++ Offer06/Offer06.java
@@ -18,6 +18,29 @@
  */
 package Offer06;
 
-public class Offer06 {
+import java.util.*;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Offer06 {
+    public int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = stack.pop().val;
+        }
+        return array;
+    }
 }
