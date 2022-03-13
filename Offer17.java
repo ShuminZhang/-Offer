1f1ab7175f6b09fc7986dcc3e8c4647c2451ca61
Ciaran <mgqclseyn@163.com>
2022-03-13 19:05:04 +0800

    pow

---
+++ Offer17.java
@@ -0,0 +1,10 @@
+public class Offer17 {
+    public int[] printNumbers(int n) {
+        int end = (int) Math.pow(10, n) - 1;
+        int[] arr = new int[end];
+        for (int i = 0; i < end; i++) {
+            arr[i] = i + 1;
+        }
+        return arr;
+    }
+}
